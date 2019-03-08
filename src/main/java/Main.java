43313f8897b004d87c4b1489c3c72b5284de9b47
melnychuk yurii <uronon23@gmail.com>
2019-03-08 12:14:50 +0200

    added new class Block

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    ArrayList<Block> blockchain = new ArrayList<Block>();
+
+    public static void main(String[] args) {
+
+        String[] genesisTransactions = {"send 1000 bitcoins"};
+        Block genesisBlock = new Block(0, genesisTransactions);
+
+        String[] block2Transactions = {"send 10 bitcoin to satoshi"};
+
+        Block block2 = new Block(genesisBlock.getBlockHash(), block2Transactions);
+
+        System.out.println("Hash of genesis block");
+        System.out.println(genesisBlock.getBlockHash());
+        System.out.println("Hash of block2:");
+        System.out.println(block2.getBlockHash());
+
+
+    }
+}
